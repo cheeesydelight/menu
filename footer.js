12a74ce4d530b408a11de413fb230600d99ebf1a
cheeesydelight <cheeesydelight0@gmail.com>
2025-07-07 19:14:14 +0530

    footer change

--- footer.js
+++ footer.js
@@ -1,8 +1,9 @@
 document.getElementById('footer-container').innerHTML = `
   <footer class="site-footer">
     <p>&copy; <span id="year"></span> <strong>cyberpeshwas</strong> · All rights reserved</p>
-    <p>📞 <a href="tel:+918180858531">+91 81808 58531</a> |
-       💬 <a href="https://wa.me/918180858531" target="_blank">Chat on WhatsApp</a></p>
+    <p>📞 <a href="tel:+918087408986">+91 80874 08986</a> |
+      📞 <a href="tel:+918180858531">+91 81808 58531</a> |
+       💬 <a href="https://wa.me/918087408986" target="_blank">Chat on WhatsApp</a></p>
   </footer>
 `;
 
